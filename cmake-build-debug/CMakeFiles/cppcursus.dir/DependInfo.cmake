59b9807e4288d492de2abfa45cb74ed57ef5a30f
Finn Hamers <m.hamers97@gmail.com>
2018-02-26 13:32:42 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/cppcursus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/parallels/Documents/cppcourse/game.cpp" "/home/parallels/Documents/cppcourse/cmake-build-debug/CMakeFiles/cppcursus.dir/game.cpp.o"
+  "/home/parallels/Documents/cppcourse/surface.cpp" "/home/parallels/Documents/cppcourse/cmake-build-debug/CMakeFiles/cppcursus.dir/surface.cpp.o"
+  "/home/parallels/Documents/cppcourse/template.cpp" "/home/parallels/Documents/cppcourse/cmake-build-debug/CMakeFiles/cppcursus.dir/template.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cppcursus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppcursus.dir/game.cpp.o"
+  "CMakeFiles/cppcursus.dir/surface.cpp.o"
+  "CMakeFiles/cppcursus.dir/template.cpp.o"
+  "cppcursus.pdb"
+  "cppcursus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppcursus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
